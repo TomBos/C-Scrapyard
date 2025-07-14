e2ab2212c27f790602a8f5dff270bec88a91f480
TomBos <tombos255@gmail.com>
2025-07-14 08:54:10 +0200

    Template for 4th problem

---
+++ 001-terminal-input-and-output/004.c
@@ -0,0 +1,29 @@
+/*
+=====================  ASSIGNMENT  =====================
+
+Write a program that adds a 25% tax to a given price.
+
+Input:	Enter the price without tax: 100
+Output:	Sales price with tax (25%): 125
+
+=========================================================
+*/
+
+#include <stdio.h>
+
+int main() {
+	int c;
+	int i = 0;
+	int dec = 0;
+
+	printf("%s","Input:  Enter the price without tax: ");
+	while ((c = getchar()) != EOF && c != '\n' && i < 15) {
+		if (c >= '0' && c <= '9') {
+			dec = dec * 10 + (c - '0');
+			i++;
+		}
+	}
+
+	printf("Output: %d\n", dec);
+	return 0;
+}
