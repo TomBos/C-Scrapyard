1248268108706730a4e8bcc8b73b6b816f9b3dd4
TomBos <tombos255@gmail.com>
2025-07-18 08:57:52 +0200

    Assignment 5 completed

--- 001-terminal-input-and-output/005.c
+++ 001-terminal-input-and-output/005.c
@@ -8,4 +8,32 @@ Output:	Rectangle with length 5 and width 4 has area 20
 
 =========================================================
 */
+#include <stdio.h>
+
+int main() {
+	int c;
+
+	// Parsing length
+	int pl = 1;
+	int l = 0, w = 0;
+
+	printf("%s","Input:  Enter the length and width: ");
+	while ((c = getchar()) != EOF && c != '\n') {
+		if (c == ' ') {
+			pl = 0;	
+			continue;	
+		}
+
+		if (c >= '0' && c <= '9') {
+			if (pl) {
+				l = l * 10 + (c - '0');
+			} else {
+				w = w * 10 + (c - '0');
+			}
+		}
+	}
+
+	printf("Output: Rectangle with length %d and width %d has area %d\n", l, w, l * w);
+	return 0;
+}
 
